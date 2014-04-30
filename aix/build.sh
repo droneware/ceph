60b36e55e24a9001dfefba00242226563b4e2891
droneware <code@droneware.com>
2014-04-30 15:01:34 -0400

    added librados-config to aix package

--- aix/build.sh
+++ aix/build.sh
@@ -2,12 +2,14 @@
 
 rm -rf pkg-build
 mkdir -p pkg-build/usr/lib
+mkdir -p pkg-build/usr/bin
 mkdir -p pkg-build/usr/include/rados
 
 cp ../src/.libs/librados.a pkg-build/usr/lib/.
 cp ../src/.libs/librados.so.2 pkg-build/usr/lib/.
 cp ../src/.libs/librados.la pkg-build/usr/lib/.
 
+cp ../src/.libs/librados-config pkg-build/usr/bin/.
 cp ../src/include/rados/buffer.h pkg-build/usr/include/rados
 cp ../src/include/rados/crc32c.h pkg-build/usr/include/rados
 cp ../src/include/rados/librados.h pkg-build/usr/include/rados
